5cc57af788b30ade3b281f025e984fcce036dd25
Scott G <scottg489@gmail.com>
2016-03-09 15:52:54 +0000

    Change idea64.vmoptions back to defaults.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,7 @@
 default['idea']['setup_dir'] = '/opt/idea'
 default['idea']['version'] = '15.0.4'
+# TODO: I don't like how the cookbook in general is overwriting the default idea64.vmoptions file
+default['idea']['64bits']['Xms'] = '128m'
+default['idea']['64bits']['Xmx'] = '750m'
+
 default['java']['jdk_version'] = '7'
