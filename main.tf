8b884d1c98f6dce3a4163bb1c9d034f2f7097416
Scott Giminiani <scottg489@gmail.com>
2020-06-14 16:48:54 -0700

    Add terraform config to create an ec2 instance for testing
    In order to test the changes to the machine setup are working properly
    we want to be able to run it regularly against an actual machine. This
    allows us to provision an ec2 instance to run the playbook against to
    verify it's working properly

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-003634241a8fcdec0"
+  instance_type = "t2.small"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  key_name = aws_key_pair.machine_prov_test_key.key_name
+
+  tags = {
+    Name = "machine-prov-test"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "machine-prov-test-sg"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "machine_prov_test_key" {
+  key_name   = "machine-prov-test-key"
+  public_key = var.public_key
+}
