3232f61b3a880187c855bc7e3f51fc876fa6f0f1
Scott G <scottg489@gmail.com>
2020-06-27 02:37:03 -0700

    Use wget instead of curl to call the build server
    This will allow the build to fail when the server returns an
    unsuccessful response code.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-curl -v -s -f --data-binary '{"ID_RSA": "'"$1"'", "AWS_CREDENTIALS": "'"$2"'", "MAIN_KEY_PAIR": "'"$3"'"}' 'http://simple-ci.com/build?image=scottg489/machine-setup-build:latest'
+wget --content-on-error -nv -O - --post-data  '{"ID_RSA": "'"$1"'", "AWS_CREDENTIALS": "'"$2"'", "MAIN_KEY_PAIR": "'"$3"'"}' 'http://simple-ci.com/build?image=scottg489/machine-setup-build:latest'
