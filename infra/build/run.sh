852e1db8c4c1693e44673e2a328f9eb31c9eeb71
Scott G <scottg489@gmail.com>
2020-07-09 02:17:34 -0700

    Use existing file rather than printing envar
    This way is a bit cleaner since we aren't accessing secrets outside of
    the setup_credentials function. This is particularly dangerous because
    we can accidentally print out secrets in the build log.

--- infra/build/run.sh
+++ infra/build/run.sh
@@ -10,9 +10,6 @@ cleanup() {
 
 set +x
 setup_credentials "$1"
-# TODO: Kinda hacky since we already do this parsing in above function.
-declare _ID_RSA_CONTENTS
-readonly _ID_RSA_CONTENTS=$(echo -n $1 | jq -r .ID_RSA | base64 --decode)
 set -x
 
 declare -r _PROJECT_NAME='machine-setup'
@@ -25,8 +22,8 @@ cd $_PROJECT_NAME
 # TODO:   is expecting it. However, we should really be passing this in as a separate input to
 # TODO:   this build. By doing it this way we are assuming the RSA key to clone the repo is the
 # TODO:   same as the one we want to put onto the provisioned machine.
-# TODO: Can we actually just copy this from /root? Just need to make sure permissions are right
-printf -- "$_ID_RSA_CONTENTS" > files/ssh/id_rsa
+cp /root/.ssh/id_rsa files/ssh/id_rsa
+chmod 644 files/ssh/id_rsa
 
 tf_apply "infra/tf"
 
