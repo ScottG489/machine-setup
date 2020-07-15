106940bf1efbc416615a973a25776a5edcaad18b
Scott G <scottg489@gmail.com>
2020-07-15 00:40:06 -0700

    Use published terraform modules
    These modules are shared between a few of my projects and have been put
    in 1 place to keep things simpler.

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -2,9 +2,14 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "spot_instance_ssh" {
-  source = "./modules/spot_instance_ssh"
+module "helpers_spot_instance_ssh" {
+  source = "ScottG489/helpers/aws//modules/spot_instance_ssh"
+  version = "0.0.1"
   name = random_id.name.hex
+  instance_type = var.instance_type
+  spot_type = var.spot_type
+  spot_price = var.spot_price
+  volume_size = var.volume_size
   public_key = var.public_key
 }
 
