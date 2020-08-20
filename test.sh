da82382de563251d7dd7a09fdc72d0104e151565
Scott G <scottg489@gmail.com>
2020-08-20 14:20:20 -0700

    Add script for local development testing
    By running this script you are essentially running the same thing that
    runs on the build server. This is a good way to be sure the changes
    you're making work.
    
    - Document how to use the test script

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+
+declare ID_RSA_CONTENTS_BASE64
+declare AWS_CREDENTIALS_CONTENTS_BASE64
+declare MAINKEYPAIR_CONTENTS_BASE64
+# Change the location of these files based on where they are on your system
+ID_RSA_CONTENTS_BASE64=$(base64 ~/.ssh/id_rsa | tr -d '\n') ;
+AWS_CREDENTIALS_CONTENTS_BASE64=$(base64 ~/.aws/credentials | tr -d '\n') ;
+MAINKEYPAIR_CONTENTS_BASE64=$(base64 ~/.ssh/mainkeypair.pem | tr -d '\n') ;
+[[ -n $ID_RSA_CONTENTS_BASE64 ]]
+[[ -n $AWS_CREDENTIALS_CONTENTS_BASE64 ]]
+[[ -n $MAINKEYPAIR_CONTENTS_BASE64 ]]
+
+docker build infra/build -t machine-setup-test && \
+docker run -it --volume "$PWD:/opt/build/machine-setup" machine-setup-test '{"ID_RSA": "'"$ID_RSA_CONTENTS_BASE64"'", "AWS_CREDENTIALS": "'"$AWS_CREDENTIALS_CONTENTS_BASE64"'", "MAIN_KEY_PAIR": "'"$MAINKEYPAIR_CONTENTS_BASE64"'"}'
