a872c6429509a64ed9ca72d6554dd859ec7da31a
Scott Giminiani <scottg489@gmail.com>
2022-03-13 00:51:55 -0500

    Assign desired HAOS version to a variable in the HAOS install script
    This way updating the version will be easier. We already updated the
    version and missed all locations where it should have been updated.

--- roles/home-assistant-server/files/create-haos-vm.sh
+++ roles/home-assistant-server/files/create-haos-vm.sh
@@ -3,20 +3,23 @@ set -ex
 
 [[ -n "$1" ]] && VM_MEMORY=$1 || VM_MEMORY=4096
 
+readonly HAOS_VERSION=7.2
+
 cd
 mkdir -p ~/home-assistant
 cd home-assistant
 
 # Note that latest version may differ
-wget --progress=bar:force:noscroll https://github.com/home-assistant/operating-system/releases/download/7.2/haos_ova-7.2.vmdk.zip
-unzip haos_ova-6.6.vmdk.zip
+wget --progress=bar:force:noscroll \
+https://github.com/home-assistant/operating-system/releases/download/$HAOS_VERSION/haos_ova-$HAOS_VERSION.vmdk.zip
+unzip haos_ova-$HAOS_VERSION.vmdk.zip
 
 VM_UUID=$(vboxmanage createvm --name haos-vm --default --register --ostype Linux_64 | grep UUID | awk '{print $2}')
 NET_DEVICE=$(ip link | grep -v 'lo\|vir\|wl\|^[^0-9]' | awk '{print $2}' | sed 's/.$//' | head -1)
 #BLUETOOTH_USB_DEVICE_VENDOR_ID=$(vboxmanage list usbhost | grep VendorId | head -1 | awk '{print $2}')
 #BLUETOOTH_USB_DEVICE_PRODUCT_ID=$(vboxmanage list usbhost | grep ProductId | head -1 | awk '{print $2}')
 vboxmanage modifyvm "$VM_UUID" --firmware efi --nic1 bridged --bridgeadapter1 "$NET_DEVICE" --memory "$VM_MEMORY"
-vboxmanage storageattach "$VM_UUID" --storagectl IDE --port 0 --device 0 --type hdd --medium haos_ova-6.6.vmdk
+vboxmanage storageattach "$VM_UUID" --storagectl IDE --port 0 --device 0 --type hdd --medium haos_ova-$HAOS_VERSION.vmdk
 #vboxmanage usbfilter add 0 --name "Bluetooth USB device filter" --target haos-vm --vendorid "$BLUETOOTH_USB_DEVICE_VENDOR_ID" --productid "$BLUETOOTH_USB_DEVICE_PRODUCT_ID"
 
 #vboxmanage startvm --type headless "$VM_UUID"
